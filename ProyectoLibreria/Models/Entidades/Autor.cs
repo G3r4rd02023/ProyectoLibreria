9067ef908a9eeaf5d8e3811e56cccd849236893d
Gerardo Lanza <glanza007@gmail.com>
2023-11-08 10:25:55 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoLibreria/Models/Entidades/Autor.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoLibreria.Models.Entidades
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Nombre { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}")]
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
